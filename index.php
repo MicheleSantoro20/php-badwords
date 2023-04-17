880a74cc4df5237dd6f6d4792eced053cce03dd1
Michele Santoro <michelesantoro2000@icloud.com>
2023-04-17 14:35:14 +0200

    Aggiunte stilizzazioni grafiche

--- index.php
+++ index.php
@@ -5,17 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <form action="./receive.php" method="GET">
-        
-        <label for="paragraph">Paragrafo:</label>
-        <input type="text" placeholder="Scrivi il tuo paragrafo" name="paragraph">
-        <label for="censured">Parola da censurare:</label>
-        <input type="text" placeholder=" Scrivi la parola da censurare" name="censured">
+    <div class="container">
+        <form action="./receive.php" method="GET">
 
-        <button type="submit">Invia Dati</button>
+            <div>
+                <label for="paragraph">Paragrafo:</label>
+                <input type="text" placeholder="Scrivi il tuo paragrafo" name="paragraph">
+            </div>
 
-    </form>
+            <div>
+                <label for="censured">Parola da censurare:</label>
+                <input type="text" placeholder=" Scrivi la parola da censurare" name="censured">
+            </div>
+
+
+            <button type="submit">Invia Dati</button>
+
+        </form>
+    </div>
+
 </body>
 </html>
--- receive.php
+++ receive.php
@@ -12,14 +12,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Badwords Response</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 
-    <h1><?php echo $paragraph ?></h1>
-    <h1><?php echo $paragraph_length ?></h1>
-    <h1><?php echo $censured_paragraph ?></h1>
-    <h1><?php echo $censured_paragraph_length ?></h1>
+    <div class="container">
+        <h1><?php echo $paragraph ?></h1>
+        <h1>Lunghezza: <span><?php echo $paragraph_length ?></span></h1>
+        <h1><?php echo $censured_paragraph ?></h1>
+        <h1>Lunghezza: <span><?php echo $censured_paragraph_length ?></span></h1>
+    </div>
+
 
 </body>
 </html>
